9bc782a76895d8189005b918f8bf43e2d3b5db2f
althaaf <althaaf@LAPTOP-09LJ77LS>
2025-09-09 19:31:13 +0530

    git

---
+++ samplespring/src/main/java/com/tnsiff/samplespring/Clothes.java
@@ -0,0 +1,9 @@
+package com.tnsiff.samplespring;
+
+public class Clothes implements Shopping{
+	
+	public void order() {
+		System.out.println("Im Ordering some dress");
+	}
+
+}
---
+++ samplespring/src/main/java/com/tnsiff/samplespring/Shopping.java
@@ -0,0 +1,6 @@
+package com.tnsiff.samplespring;
+
+public interface Shopping {
+	void order();
+
+}
