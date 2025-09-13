1618ea9adc7b04c18a6d5da31d004504cf1bd743
althaaf123 <syedalthaaf4@gmail.com>
2025-09-13 13:34:24 +0530

    commit

---
+++ StudenServices/src/main/java/com/tnsif/Student/StudenServicesApplication.java
@@ -0,0 +1,13 @@
+package com.tnsif.Student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudenServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudenServicesApplication.class, args);
+	}
+
+}
